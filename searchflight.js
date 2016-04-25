83d0068db49835d65fb8efe160f882bb1513c2af
aliasgher53 <ali.asgher5253@gmail.com>
2016-04-25 15:12:17 +1000

    Added files via upload

---
+++ searchflight.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+
+    $('#flight-from').keyup(function () {
+       alert('asdsa');
+    });
+    
+    $('#flight-to').keyup(function () {
+       alert('asdsa');
+    });
+});
+
